0af50c76c810f908670f3c032b9265846f700674
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-06-25 18:34:57 +0200

    add npm slim-select

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+const slimSelect = new SlimSelect({
+  select: '#single',
+  settings: {
+    showSearch: false,
+  },
+});
 
 const breedSelectEl = document.querySelector('.breed-select');
 const errorEl = document.querySelector('.error');
@@ -6,15 +14,14 @@ const catInfoEl = document.querySelector('.cat-info');
 
 fetchBreeds()
   .then(data => {
-    const breeds = data
-      .map(({ id, name }) => {
-        return `<option value="${id}">${name}</option>`;
-      })
-      .join('');
-    breedSelectEl.insertAdjacentHTML('beforeend', breeds);
+    const cats = data.map(({ id, name }) => {
+      return { text: name, value: id };
+    });
+    slimSelect.setData([...cats]);
     breedSelectEl.classList.add('visibility');
     errorEl.classList.remove('visibility');
   })
+
   .catch(error => {
     breedSelectEl.classList.remove('visibility');
     errorEl.classList.add('visibility');
@@ -23,15 +30,16 @@ fetchBreeds()
 breedSelectEl.addEventListener('change', e => {
   fetchCatByBreed(e.currentTarget.value)
     .then(data => {
-      console.log(data);
       const { url, breeds } = data;
       const { name, description, temperament } = breeds[0];
-      console.log(name);
+      catInfoEl.innerHTML = '';
+
       catInfoEl.innerHTML = `<img src="${url}" alt="" class="cat-info__img" width="40%"/>
-   <div class="cat-info__wrapper">
-   <h1 class="cat-info__title">${name}</h1>
-      <p class="cat-info__description">${description}</p>
-      <p class="cat-info__temperament">${temperament}</p></div>`;
+      <div class="cat-info__wrapper">
+        <h1 class="cat-info__title">${name}</h1>
+        <p class="cat-info__description">${description}</p>
+        <p class="cat-info__temperament">${temperament}</p>
+      </div>`;
     })
     .catch(error => {
       breedSelectEl.classList.remove('visibility');
