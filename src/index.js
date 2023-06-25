33a5a0f2c7b7ab4aa4f54762b8ff3030560cee0a
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-06-25 17:18:35 +0200

    add fn fetchCatByBreed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-import './js/cat-api';
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
+
+const breedSelectEl = document.querySelector('.breed-select');
+const errorEl = document.querySelector('.error');
+const catInfoEl = document.querySelector('.cat-info');
+
+fetchBreeds()
+  .then(data => {
+    const breeds = data
+      .map(({ id, name }) => {
+        return `<option value="${id}">${name}</option>`;
+      })
+      .join('');
+    breedSelectEl.insertAdjacentHTML('beforeend', breeds);
+    breedSelectEl.classList.add('visibility');
+    errorEl.classList.remove('visibility');
+  })
+  .catch(error => {
+    breedSelectEl.classList.remove('visibility');
+    errorEl.classList.add('visibility');
+  });
+
+breedSelectEl.addEventListener('change', e => {
+  fetchCatByBreed(e.currentTarget.value).then(data => {
+    console.log(data);
+    const { url } = data;
+    catInfoEl.innerHTML = `<img src="${url}" alt="" width="50%"/>
+      <h1></h1>
+      <p></p>
+      <p></p>`;
+  });
+});
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,27 +1,16 @@
 import axios from 'axios';
 import './axiosAPI';
 
-const breedSelectEl = document.querySelector('.breed-select');
-const errorEl = document.querySelector('.error');
+export async function fetchBreeds() {
+  const breeds = await axios.get('breeds');
+  const data = await breeds.data;
 
-async function fetchBreeds() {
-  try {
-    const breeds = await axios.get('breeds');
-    const data = await breeds.data;
-
-    const options = data
-      .map(({ id, name }) => {
-        return `<option value=${id}>${name}</option>`;
-      })
-      .join('');
-
-    breedSelectEl.insertAdjacentHTML('beforeend', options);
-    breedSelectEl.classList.add('visibility');
-    errorEl.classList.remove('visibility');
-  } catch (error) {
-    breedSelectEl.classList.remove('visibility');
-    errorEl.classList.add('visibility');
-  }
+  return data;
 }
 
-fetchBreeds();
+export async function fetchCatByBreed(breedId) {
+  const catInfo = await axios.get(`images/search?breed_ids=${breedId}`);
+  const data = await catInfo.data[0];
+
+  return data;
+}
